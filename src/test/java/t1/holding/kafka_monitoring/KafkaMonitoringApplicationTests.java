d81b9748d040e6a4b3374c13a496c22e073cf240
mgfallen <minegoodfeeling@gmail.com>
2024-09-05 21:46:23 +0300

    init skeleton commit

---
+++ src/test/java/t1/holding/kafka_monitoring/KafkaMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package t1.holding.kafka_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
