7942917c0ff1d504449a9bf5cea133ed0d175ecd
mgfallen <minegoodfeeling@gmail.com>
2024-09-05 22:01:11 +0300

    created 2 services - producer and consumer

---
+++ metrics-consumer/src/main/java/t1/holding/metrics_consumer/MetricsConsumerApplication.java
@@ -0,0 +1,13 @@
+package t1.holding.metrics_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsConsumerApplication.class, args);
+	}
+
+}
---
+++ metrics-producer/src/test/java/t1/holding/metrics_producer/MetricsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package t1.holding.metrics_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
